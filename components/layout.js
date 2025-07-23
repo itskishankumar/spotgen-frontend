5c3e459e86db22c3d28ed1ee5a073cab9e39ec81
Kishan Kumar <itskishankumar98@gmail.com>
2025-07-24 00:58:20 +0530

    Remove react-detect-offline library and main_content_wrapper.js

--- components/layout.js
+++ components/layout.js
@@ -1,12 +1,11 @@
 import SideBar from './side_bar'
-import MainContentWrapper from './main_content_wrapper'
 
 export default function Layout({ children }) {
   return (
     <div className='h-screen w-screen flex flex-row'>
       <SideBar />
-      <main className='flex-1 w-px bg-gradient-to-br from-black to-blue-900 p-10 max-h-screen overflow-y-auto scrollbar scrollbar-thumb-blue-700 scrollbar-track-gray-200'>
-        <MainContentWrapper props={children} />
+      <main className='h-full flex-1 w-px bg-gradient-to-br from-black to-blue-900 p-10 max-h-screen overflow-y-auto scrollbar scrollbar-thumb-blue-700 scrollbar-track-gray-200'>
+        {children}
       </main>
     </div >
   )
